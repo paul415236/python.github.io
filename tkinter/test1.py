c120eaf51e9156f6d74b3a65cd0a25caecc43e3e
Paul.Lin <paul415236@gmail.com>
2017-08-27 22:55:58 -0400

    add button and activate certain function by button

--- tkinter/test1.py
+++ tkinter/test1.py
@@ -9,15 +9,38 @@ win.title('tkinter window')
 # size of window
 win.geometry('200x100')
 
+var = tk.StringVar()
+
 # label
 label = tk.Label(	win, 
-					text = 'Hello !',
+					#text = 'Hello !',
+					textvariable = var,
 					bg = 'green',
 					font = ('Arial', 12),
 					width = 15, height = 2
 				)
-
 # put the label
 label.pack()
+
+
+clicked = False
+def click_button():
+	global clicked
+	if clicked == False:
+		clicked = True
+		var.set('confirmed')
+	else:
+		clicked = False
+		var.set('')
+
+button = tk.Button(	win,
+					text = 'ok',
+					width = 15, height = 2,
+					command = click_button
+					)
+# put the button
+button.pack()
+
+
 
 win.mainloop()
