fd1cd0076cbfffe98026bf4d67848911cf33abb4
Paul.Lin <paul415236@gmail.com>
2017-08-27 23:01:12 -0400

    code revise

--- tkinter/test1.py
+++ tkinter/test1.py
@@ -12,13 +12,13 @@ win.geometry('200x100')
 var = tk.StringVar()
 
 # label
-label = tk.Label(	win, 
-					#text = 'Hello !',
-					textvariable = var,
-					bg = 'green',
-					font = ('Arial', 12),
-					width = 15, height = 2
-				)
+label = tk.Label(   win, 
+                    #text = 'Hello !',
+                    textvariable = var,
+                    bg = 'green',
+                    font = ('Arial', 12),
+                    width = 15, height = 2
+                )
 # put the label
 label.pack()
 
@@ -33,11 +33,11 @@ def click_button():
 		clicked = False
 		var.set('')
 
-button = tk.Button(	win,
-					text = 'ok',
-					width = 15, height = 2,
-					command = click_button
-					)
+button = tk.Button( win,
+                    text = 'ok',
+                    width = 15, height = 2,
+                    command = click_button
+                    )
 # put the button
 button.pack()
 
